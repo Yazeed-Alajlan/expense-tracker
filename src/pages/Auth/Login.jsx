9af05b71eace6b33e44dd5ba3564fafe20791843
1Yazeed <yazeedal3jlan@hotmail.com>
2022-09-28 10:58:34 +0300

    Auth

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -32,44 +32,62 @@ const Login = () => {
 
   return (
     <CenteredContainer>
-      <Card className="shadow border-dark">
-        <Card.Title>
-          <h1 className="text-center mb-2 ">Login to Your Account</h1>
-        </Card.Title>
-        <Card.Body>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="form-input mb-3">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                ref={emailRef}
-                required
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                ref={passwordRef}
-                required
-              />
-            </Form.Group>
-            <Button disabled={loading} type="submit">
-              Log In
-            </Button>
-          </Form>
-        </Card.Body>
+      <Card className="border-0 rounded-4 shadow-sm d-flex flex-row justify-content-between p-5">
+        <div className="left-section ">
+          <Card.Title>
+            <h1 className="text-center mb-2 ">Login to Your Account</h1>
+          </Card.Title>
+          <Card.Body>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="form-input mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  ref={emailRef}
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  ref={passwordRef}
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <div className="mb-3">
+                  <label className="me-2" htmlFor="">
+                    Remember me
+                  </label>
+                  <input type="checkbox" />
+                </div>
+
+                <Button
+                  variant="main"
+                  className="w-100"
+                  disabled={loading}
+                  type="submit"
+                >
+                  Log In
+                </Button>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+        </div>
+        <div className="right-section">
+          <Form.Group>
+            <h1 className="signup-title text-center ">New Here?</h1>
+            <p className="text-center">Sign up! </p>
+            <Link className="signup-btn btn text-center py-1" to="/register">
+              Sign Up
+            </Link>
+          </Form.Group>
+        </div>
       </Card>
-      <div className="container d-flex-column align-items-center py-2 gap-2 ">
-        <h1 className="signup-title">New Here?</h1>
-        <p>Sign up! </p>
-        <Link className="signup-btn btn text-center py-1" to="/register">
-          Sign Up
-        </Link>
-      </div>
     </CenteredContainer>
   );
 };
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import { useAuth } from "contexts/AuthContext";
 import { useCategories } from "contexts/CategoriesContext";
-import "styles/css/Auth.css";
-import { useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import CenteredContainer from "components/Utils/CenteredContainer";
+
 import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
@@ -33,65 +34,133 @@ const Register = () => {
       addDefualtCategories();
       navigate("/home");
     } catch (error) {
-      console.log(error.message);
       setError("Falid to create an account");
     }
     setLoading(false);
   };
 
   return (
-    <div className="page d-flex-row ">
-      <div className="signup-container container card w-75 ">
-        <div className="left-section d-flex-row align-items-center">
-          <div className="container d-flex-column align-items-center py-2 gap-2 ">
-            <h1 className="login-title">Has an Account?</h1>
-            <Link className="signup-btn btn text-center py-1" to="/login">
-              Sign In
-            </Link>
-          </div>
-        </div>
-
-        <div className="right-section">
-          <h1 className="signup-title text-center mb-2">Create Free Account</h1>
-          {error && <div className="card">{error}</div>}
-
-          <form className="container d-flex-column align-items-center gap-1">
-            <input
-              type="text"
-              ref={emailRef}
-              id="email"
-              placeholder="Email"
-              maxLength="100"
-              required
-            />
-            <input
-              type="password"
-              ref={passwordRef}
-              id="password"
-              placeholder="Password"
-              maxLength="128"
-              required
-            />
-            <input
+    <CenteredContainer>
+      <Card className="border-0 rounded-4 shadow-sm d-flex flex-row justify-content-between p-5">
+        <div className="left-section ">
+          <Card.Title>
+            <h1 className="text-center mb-2 ">Sign up to XXXXX</h1>
+          </Card.Title>
+          <Card.Body>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="form-input mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  ref={emailRef}
+                  id="email"
+                  placeholder="Email"
+                  maxLength="100"
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordRef}
+                  id="password"
+                  placeholder="Password"
+                  maxLength="128"
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
               type="password"
               ref={passwordConfirmationRef}
               id="passwordConfirmation"
               placeholder="Confirm Password"
               maxLength="128"
               required
-            />
-            <button
-              className="login-btn btn my-1"
-              disabled={loading}
-              onClick={handleSubmit}
-              type="submit"
-            >
-              Sign Up
-            </button>
-          </form>
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+           
+
+                <Button
+                  variant="main"
+                  className="w-100"
+                  disabled={loading}
+                  type="submit"
+                >
+                  Creat an Account
+                </Button>
+              </Form.Group>
+            </Form>
+          </Card.Body>
         </div>
-      </div>
-    </div>
+        <div className="right-section">
+          <Form.Group>
+            <h1 className="signup-title text-center ">New Here?</h1>
+            <p className="text-center">Sign up! </p>
+            <Link className="signup-btn btn text-center py-1" to="/login">
+              Sign Up
+            </Link>
+          </Form.Group>
+        </div>
+      </Card>
+    </CenteredContainer>
+
+    // <div className="page d-flex-row ">
+    //   <div className="signup-container container card w-75 ">
+    //     <div className="left-section d-flex-row align-items-center">
+    //       <div className="container d-flex-column align-items-center py-2 gap-2 ">
+    //         <h1 className="login-title">Has an Account?</h1>
+    //         <Link className="signup-btn btn text-center py-1" to="/login">
+    //           Sign In
+    //         </Link>
+    //       </div>
+    //     </div>
+
+    //     <div className="right-section">
+    //       <h1 className="signup-title text-center mb-2">Create Free Account</h1>
+    //       {error && <div className="card">{error}</div>}
+
+    //       <form className="container d-flex-column align-items-center gap-1">
+    //         <input
+    //           type="text"
+    //           ref={emailRef}
+    //           id="email"
+    //           placeholder="Email"
+    //           maxLength="100"
+    //           required
+    //         />
+    //         <input
+    //           type="password"
+    //           ref={passwordRef}
+    //           id="password"
+    //           placeholder="Password"
+    //           maxLength="128"
+    //           required
+    //         />
+    //         <input
+    //           type="password"
+    //           ref={passwordConfirmationRef}
+    //           id="passwordConfirmation"
+    //           placeholder="Confirm Password"
+    //           maxLength="128"
+    //           required
+    //         />
+    //         <button
+    //           className="login-btn btn my-1"
+    //           disabled={loading}
+    //           onClick={handleSubmit}
+    //           type="submit"
+    //         >
+    //           Sign Up
+    //         </button>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
